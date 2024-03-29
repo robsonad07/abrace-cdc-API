904e1ed747288ad7f11505d3803a1e6402953a43
Robson <robsonad07@gmail.com>
2024-03-29 15:25:19 -0300

    Spring Boot 3 project started

---
+++ src/main/java/com/abracecdcAPI/abracecdcAPI/AbraceCdcApiApplication.java
@@ -0,0 +1,13 @@
+package com.abracecdcAPI.abracecdcAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbraceCdcApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbraceCdcApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abracecdcAPI/abracecdcAPI/AbraceCdcApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abracecdcAPI.abracecdcAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbraceCdcApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
