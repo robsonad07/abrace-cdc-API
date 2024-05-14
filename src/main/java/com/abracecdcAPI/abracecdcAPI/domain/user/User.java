2b10208550d5a9c6ac0d9936943d0eea48a2d2ac
Robson <robsonad07@gmail.com>
2024-05-14 18:52:24 -0300

    Class User criada

---
+++ src/main/java/com/abracecdcAPI/abracecdcAPI/domain/user/User.java
@@ -0,0 +1,23 @@
+package com.abracecdcAPI.abracecdcAPI.domain.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    private String name;
+    private String password;
+    private String telefone;
+    private Boolean isAdmin;
+}
