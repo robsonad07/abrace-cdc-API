8b111697d2559b68c3779cd6732da6866a9d1280
Robson <robsonad07@gmail.com>
2024-08-06 17:51:16 -0300

    Migrations implementada no projeto

---
+++ src/main/resources/db/migration/V1__create-table-users.sql
@@ -0,0 +1,8 @@
+CREATE TABLE users (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    phone VARCHAR(50),
+    role VARCHAR(50) NOT NULL
+);
