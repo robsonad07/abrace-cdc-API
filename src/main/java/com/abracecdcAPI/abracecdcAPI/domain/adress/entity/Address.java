1650c8b6b3e2526625d6084043e89522ef34d2b5
Robson <robsonad07@gmail.com>
2024-08-11 16:04:01 -0300

    Entidade Address implementada

---
+++ src/main/java/com/abracecdcAPI/abracecdcAPI/domain/adress/entity/Address.java
@@ -0,0 +1,29 @@
+package com.abracecdcAPI.abracecdcAPI.domain.adress.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Table(name = "address")
+@Entity(name = "address")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    private String cidade;
+
+    private String cep;
+
+    private String rua;
+
+    private int numero;
+
+    private String complemento;
+}
