37e7b0370140aca4c887801fb279e71205483064
gabrielmendes64 <gabriel10010100@gmail.com>
2024-08-22 22:15:59 -0300

    fix: remove unused import

--- src/main/java/com/abracecdcAPI/abracecdcAPI/domain/organizer/entity/OrganizerEntity.java
+++ src/main/java/com/abracecdcAPI/abracecdcAPI/domain/organizer/entity/OrganizerEntity.java
@@ -5,8 +5,14 @@ import java.util.UUID;
 
 import com.abracecdcAPI.abracecdcAPI.domain.event.entity.Event;
 import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
-import jakarta.persistence.*;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
