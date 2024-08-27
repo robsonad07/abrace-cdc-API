52e07a48cc61f7c78aa5b28664c785e70196a90b
gabrielmendes64 <gabriel10010100@gmail.com>
2024-08-27 17:16:24 -0300

    feat: add new validation for update organizer use case

--- src/main/java/com/abracecdcAPI/abracecdcAPI/domain/organizer/useCases/UpdateOrganizerUseCase.java
+++ src/main/java/com/abracecdcAPI/abracecdcAPI/domain/organizer/useCases/UpdateOrganizerUseCase.java
@@ -7,28 +7,44 @@ import org.springframework.stereotype.Service;
 
 import com.abracecdcAPI.abracecdcAPI.domain.organizer.entity.OrganizerEntity;
 import com.abracecdcAPI.abracecdcAPI.domain.organizer.repository.OrganizerRepository;
+import com.abracecdcAPI.abracecdcAPI.exceptions.CellphoneAlreadyExistsException;
+import com.abracecdcAPI.abracecdcAPI.exceptions.EmailAlreadyExistsException;
 import com.abracecdcAPI.abracecdcAPI.exceptions.OrganizerNotFoundException;
 
 @Service
 public class UpdateOrganizerUseCase {
-  
-  @Autowired  
+
+  @Autowired
   private OrganizerRepository organizerRepository;
 
   public OrganizerEntity execute(OrganizerEntity organizerEntityToUpdate) {
-    Optional<OrganizerEntity> organizerOptional = 
-      this.organizerRepository.findById(organizerEntityToUpdate.getId());
+    Optional<OrganizerEntity> organizerOptional = this.organizerRepository
+        .findById(organizerEntityToUpdate.getId());
 
+    Optional<OrganizerEntity> ornanizerWithSameEmail = this.organizerRepository
+        .findByEmail(organizerEntityToUpdate.getEmail());
+    
+        Optional<OrganizerEntity> ornanizerWithSameCellphone = this.organizerRepository
+        .findByCellphone(organizerEntityToUpdate.getCellphone());
+
+    if (ornanizerWithSameEmail.isPresent()) {
+      throw new EmailAlreadyExistsException();
+    }
+
+    if (ornanizerWithSameCellphone.isPresent()) {
+      throw new CellphoneAlreadyExistsException();
+    }
+    
     if (organizerOptional.isEmpty()) {
       throw new OrganizerNotFoundException();
     }
 
     var existingOrganizer = organizerOptional.get();
 
-    existingOrganizer.setName(organizerEntityToUpdate.getName());  
-    existingOrganizer.setCellphone(organizerEntityToUpdate.getCellphone());  
-    existingOrganizer.setEmail(organizerEntityToUpdate.getEmail()); 
-    
+    existingOrganizer.setName(organizerEntityToUpdate.getName());
+    existingOrganizer.setCellphone(organizerEntityToUpdate.getCellphone());
+    existingOrganizer.setEmail(organizerEntityToUpdate.getEmail());
+
     var result = organizerRepository.save(existingOrganizer);
 
     return result;
