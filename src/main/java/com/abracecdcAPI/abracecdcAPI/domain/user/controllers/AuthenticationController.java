55ac4723f4a6ecab3616fea32b68fa7d52c081fc
gabrielmendes64 <gabriel10010100@gmail.com>
2024-09-07 11:00:10 -0300

    feat: permisson for request of port 5173

--- src/main/java/com/abracecdcAPI/abracecdcAPI/domain/user/controllers/AuthenticationController.java
+++ src/main/java/com/abracecdcAPI/abracecdcAPI/domain/user/controllers/AuthenticationController.java
@@ -14,6 +14,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5173")
 public class AuthenticationController {
     @Autowired
     LoginUserUseCase loginUserUseCase;
