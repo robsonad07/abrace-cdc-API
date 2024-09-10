4b7ef7c6c60743fbb0633a577c1767ba61ed500b
gabrielmendes64 <gabriel10010100@gmail.com>
2024-09-10 13:22:12 -0300

    fix: remove unused attribute

--- src/main/java/com/abracecdcAPI/abracecdcAPI/domain/organizer/entity/OrganizerEntity.java
+++ src/main/java/com/abracecdcAPI/abracecdcAPI/domain/organizer/entity/OrganizerEntity.java
@@ -1,17 +1,12 @@
 package com.abracecdcAPI.abracecdcAPI.domain.organizer.entity;
 
-import java.util.List;
 import java.util.UUID;
-
-import com.abracecdcAPI.abracecdcAPI.domain.event.entity.Event;
-import com.fasterxml.jackson.annotation.JsonBackReference;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -38,7 +33,4 @@ public class OrganizerEntity {
   @Column(unique = true)
   private String email;
 
-  @OneToMany(mappedBy = "organizer")
-  @JsonBackReference
-  private List<Event> events;
 }
