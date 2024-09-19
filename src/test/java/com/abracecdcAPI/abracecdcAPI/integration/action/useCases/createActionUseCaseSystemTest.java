9dc09cc2dbd9e9772fefb666f56f3f035614cc19
EmanuelALemos <araujoemanuel97@gmail.com>
2024-09-19 19:32:54 -0300

    testando com o back

--- src/test/java/com/abracecdcAPI/abracecdcAPI/integration/action/useCases/createActionUseCaseSystemTest.java
+++ src/test/java/com/abracecdcAPI/abracecdcAPI/integration/action/useCases/createActionUseCaseSystemTest.java
@@ -4,8 +4,13 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 @DisplayName("Automated test of the create action function")
@@ -16,6 +21,7 @@ public class createActionUseCaseSystemTest {
         // Open Chrome
         WebDriverManager.chromedriver().setup();
         WebDriver browser = new ChromeDriver();
+        browser.manage().timeouts().implicitlyWait(Duration.ofSeconds(10)); // Ajuste o tempo conforme necessário
 
         // Open localhost abrace-cdc
         browser.get("http://localhost:5173/admin/");
@@ -25,10 +31,10 @@ public class createActionUseCaseSystemTest {
 
         // Click on the New Action button
         browser.findElement(By.xpath("//*[@id=\'root\']/div/div/div/a[2]")).click();
-        
+
         // Set the action title
         browser.findElement(By.xpath("//*[@id=\'root\']/div/div/form/div[1]/input")).sendKeys("Test Action");
-   
+
         // Set the action subtitle
         browser.findElement(By.xpath("//*[@id=\'root\']/div/div/form/div[2]/input")).sendKeys("Test Subtitle");
 
@@ -39,43 +45,59 @@ public class createActionUseCaseSystemTest {
         browser.findElement(By.xpath("//*[@id=\'root\']/div/div/form/div[4]/div[1]/button[2]")).click();
 
         // Set the category title
-        browser.findElement(By.xpath("//*[@id=\'new-category-form\']/input[1]"))
-            .sendKeys("Test Category");
+        String categoryName = "Test Category";
+        browser.findElement(By.xpath("//*[@id=\'new-category-form\']/input[1]")).sendKeys(categoryName);
 
         // Set the category subtitle
-        browser.findElement(By.xpath("//*[@id=\'new-category-form\']/input[2]"))
-            .sendKeys("Test Subtitle");
+        browser.findElement(By.xpath("//*[@id=\'new-category-form\']/input[2]")).sendKeys("Test Subtitle");
 
         // Save the new category
         browser.findElement(By.xpath("//*[@id=\'new-category-form\']/button")).click();
 
-        // Select the category
-        browser.findElement(By.xpath("//*[@id=\"root\"]/div/div/form/div[4]/div[1]/button[1]")).click();
+        // Wait for the category to be added to the select
+        WebDriverWait wait = new WebDriverWait(browser, Duration.ofSeconds(10)); // Ajuste o tempo conforme necessário
+        wait.until(ExpectedConditions.textToBePresentInElementLocated(
+                By.xpath("//*[@id='root']/div/div/form/div[4]/div[1]/select"), categoryName));
 
-        // create new organizer
+        // Select the category in the dropdown
+        WebElement categoryDropdown = browser
+                .findElement(By.xpath("//*[@id='root']/div/div/form/div[4]/div[1]/select"));
+        Select select = new Select(categoryDropdown);
+        select.selectByVisibleText(categoryName); // Seleciona a categoria criada pelo nome
+
+        // Criar novo organizador
         browser.findElement(By.xpath("//*[@id=\'root\']/div/div/form/div[4]/div[2]/button[2]")).click();
 
-        // Set the organizer name
-        browser.findElement(By.xpath("//*[@id=\'new-organizer-form\']/input[1]"))
-            .sendKeys("Test Organizer");
+        // Setar o nome do organizador
+        String organizerName = "Test Organizer";
+        browser.findElement(By.xpath("//*[@id=\'new-organizer-form\']/input[1]")).sendKeys(organizerName);
 
-        // Set the organizer email
-        browser.findElement(By.xpath("//*[@id=\'new-organizer-form\']/input[2]"))
-            .sendKeys("email@test.com");
+        // Setar o email do organizador
+        browser.findElement(By.xpath("//*[@id=\'new-organizer-form\']/input[2]")).sendKeys("test.organizer@email.com");
 
-        // Set the organizer phone
-        browser.findElement(By.xpath("//*[@id=\'new-organizer-form\']/input[3]"))
-            .sendKeys("123456789");
+        // Setar o telefone do organizador
+        browser.findElement(By.xpath("//*[@id=\'new-organizer-form\']/input[3]")).sendKeys("777777777");
 
-        // Save the new organizer
+        // Salvar o novo organizador
         browser.findElement(By.xpath("//*[@id=\'new-organizer-form\']/button")).click();
 
-        // Select the organizer
-        browser.findElement(By.xpath("//*[@id=\"root\"]/div/div/form/div[4]/div[2]/button[1]")).click();
+        // Aguarde até que o organizador seja adicionado ao select
+        WebDriverWait waitOrganizer = new WebDriverWait(browser, Duration.ofSeconds(10)); // Ajuste o tempo conforme
+                                                                                          // necessário
+        waitOrganizer.until(ExpectedConditions.textToBePresentInElementLocated(
+                By.xpath("//*[@id='root']/div/div/form/div[4]/div[2]/select"), organizerName));
+
+        // Localizar o dropdown de organizadores
+        WebElement organizerDropdown = browser
+                .findElement(By.xpath("//*[@id='root']/div/div/form/div[4]/div[2]/select"));
+        Select selectOrganizer = new Select(organizerDropdown);
+
+        // Selecionar o organizador pelo nome visível
+        selectOrganizer.selectByVisibleText(organizerName); // Seleciona o organizador criado pelo nome
 
         // Set the action description
         browser.findElement(By.xpath("//*[@id=\'root\']/div/div/form/div[5]/textarea"))
-            .sendKeys("Test Description");
+                .sendKeys("Test Description");
 
         // Save the action
         browser.findElement(By.xpath("//*[@id=\'root\']/div/div/form/button")).click();
